d88ea5078afe416b9d07e42efa00bcbc6acd22f0
杂鱼77 <2502786935@qq.com>
2025-10-12 19:15:15 +0800

    Fourth commit,双操作数操作符处理

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,6 +16,10 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->btnNum7,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
     connect(ui->btnNum8,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
     connect(ui->btnNum9,SIGNAL(clicked()),this,SLOT(btnNumClicked()));
+    connect(ui->btnMultiple,SIGNAL(clicked()),this,SLOT(binaryOperatorClicked()));
+    connect(ui->btnPlus,SIGNAL(clicked()),this,SLOT(binaryOperatorClicked()));
+    connect(ui->btnMinus,SIGNAL(clicked()),this,SLOT(binaryOperatorClicked()));
+    connect(ui->btnDivide,SIGNAL(clicked()),this,SLOT(binaryOperatorClicked()));
 }
 
 MainWindow::~MainWindow()
@@ -23,25 +27,88 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+QString MainWindow::calculation(bool *ok)
+{
+    double ret=0;
+    if(operands.size()==2&&opcode.size()){
+        double operand1=operands.front().toDouble();//获取第一个操作数
+        operands.pop_front();//这栈怎么跟双向队列一样
+        double operand2=operands.front().toDouble();//获取第二个操作数
+        operands.pop_front();
+        QString op=opcode.front();//取得一个操作符
+        opcodes.pop_front();
+        //进行运算
+        if(op=="+"){
+            ret=operand1+operand2;
+        }
+        else if(op=="-"){
+            ret=operand1-operand2;
+        }
+        else if(op=="*"){
+            ret=operand1*operand2;
+        }
+        else if(op=="/"){
+            ret=operand1/operand2;
+        }
+    }
+    else {
+        ui->statusbar->showMessage(QString("operands is %1,opcode is %2").arg(operands.size()).arg(opcode.size()));
+    }
+    return QString::number(ret);
+}
+
 void MainWindow::btnNumClicked()
 {
-    QString str=ui->display->text();
-    str=str+qobject_cast<QPushButton*>(sender())->text();//准确获取发送信息的对象,因为有很多数字按钮需要区分
-    ui->display->setText(str);
+    QString digit=qobject_cast<QPushButton*>(sender())->text();//准确获取发送信息的对象,因为有很多数字按钮需要区分
+    if(digit=="0"&&operand=="0") digit="";
+    if(operand=="0"&&digit!="0") operand="";
+    operand+=digit;
+    ui->display->setText(operand);
+}
+
+void MainWindow::binaryOperatorClicked()
+{
+    opcode=qobject_cast<QPushButton*>(sender())->text();//记录操作符
+    //点击操作符之后让操作数入栈
+    if(operand!="") {
+        operands.push_back(operand);
+        operand="";
+        opcodes.push_back(opcode);
+    }
+    QString ret=calculation();
+    ui->display->setText(ret);//可能出现连加，连减等
 }
 
 void MainWindow::on_btnPeriod_clicked()
 {
-    QString str=ui->display->text();
-    if(!str.contains(".")) str=str+".";
-    ui->display->setText(str);
+    if(!operand.contains(".")) operand=operand+".";
+    ui->display->setText(operand);
 }
 
 
 void MainWindow::on_btnDel_clicked()
 {
-    QString str=ui->display->text();
-    str=str.left(str.length()-1);//从左边开始截取这么多
-    ui->display->setText(str);
+    operand=operand.left(operand.length()-1);//从左边开始截取这么多
+    ui->display->setText(operand);
 }
+
+
+void MainWindow::on_btnClearAll_clicked()
+{
+    operand.clear();
+    ui->display->setText(operand);
+}
+
+
+void MainWindow::on_btnEqual_clicked()
+{
+    if(operand!="") {
+        operands.push_back(operand);
+        operand="";
+        QString ret=calculation();
+        ui->display->setText(ret);
+    }
+}
+
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<QStack>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,12 +18,25 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    QString operand;
+    QString opcode;
+    QStack<QString>operands;
+    QStack<QString>opcodes;
+
+    QString calculation(bool *ok=NULL);
+
 private slots:
     void btnNumClicked();
+
+    void binaryOperatorClicked();
 
     void on_btnPeriod_clicked();
 
     void on_btnDel_clicked();
+
+    void on_btnClearAll_clicked();
+
+    void on_btnEqual_clicked();
 
 private:
     Ui::MainWindow *ui;
